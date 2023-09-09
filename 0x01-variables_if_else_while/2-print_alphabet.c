fb537ee6dd9d32cea4435048d5c519ffc354dab8
Munonyezubby <munex67@gmail.com>
2023-09-09 09:25:08 -0700

    program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - tis is the main function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char p;
+
+	for (p = 'a'; p <= 'z'; p++)
+	putchar(p);
+	putchar('\n');
+	return (0);
+}
