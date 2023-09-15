c80ed6597992e7cc39accbbdaa494cd6d2b290cf
Munonyezubby <munex67@gmail.com>
2023-09-15 16:10:31 -0700

    program

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print  numbers 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, k;
+
+	for (i = 1; i <= 10; i++)
+	{
+	for (k = 0; k <= 14; k++)
+	{
+	if (j >= 10)
+	_putchar('1');
+	_putchar (k % 10 + '0');
+	}
+	_putchar('\n');
+	}
+}
+
