87eda428811faea1f605a3fc8dbbc36d493f91a0
Munonyezubby <munex67@gmail.com>
2023-09-18 17:16:03 -0700

    file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - function name for task 1
+ * @a: first parameter
+ * @b: second parameter
+ */
+
+
+void swap_int(int *a, int *b)
+{
+	int k;
+k = *a;
+*a = *b;
+*b = k;
+}
