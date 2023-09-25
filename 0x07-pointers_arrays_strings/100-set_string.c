346184c14ed5dabbc7ef652b1948bda4bea60f31
Munonyezubby <munex67@gmail.com>
2023-09-25 10:21:16 -0700

    file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - main function for task 8
+  *
+  * @s: Function parameter
+  *
+  * @to: Function parameter
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
